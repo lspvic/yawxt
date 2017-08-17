04de838f7766919e528de7d1da653b35d94cc898
lspvic <lspvic@qq.com>
2017-08-17 14:32:28 +0800

    Refact setup.py with pep8

--- setup.py
+++ setup.py
@@ -7,21 +7,21 @@ with codecs.open("README.rst", "r", "utf8") as f:
     readme = f.read()
 
 setup(
-    name = 'yawxt',
-    version = '0.1.dev1',
-    packages = ['yawxt',],
-    author = 'lspvic',
-    author_email = 'lspvic@qq.com',
-    install_requires = [
+    name='yawxt',
+    version='0.1.dev1',
+    packages=['yawxt', ],
+    author='lspvic',
+    author_email='lspvic@qq.com',
+    install_requires=[
         'requests_oauthlib',
         'pyOpenSSL;python_version=="2.6"'
     ],
-    url = 'http://github.com/lspvic/yawxt',
-    license = 'MIT License',
-    description = '又一个微信开发工具箱 Yet Another WeiXin Tookit',
-    long_description = readme,
+    url='http://github.com/lspvic/yawxt',
+    license='MIT License',
+    description='又一个微信开发工具箱 Yet Another WeiXin Tookit',
+    long_description=readme,
     keywords=['wechat', 'weixin', '微信'],
-    classifiers  = [
+    classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: Chinese',
         'License :: OSI Approved :: MIT License',
@@ -34,4 +34,4 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-    ) 
+    )
